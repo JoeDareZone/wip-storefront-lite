fbb193ddba5a9335f5b1b13bb43eb80725ada5ce
Joe Ulyatt <joewhocodes@gmail.com>
2025-04-02 16:41:21 +0200

    feat: :sparkles: Add mock data

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 
+const productRoutes = require('./routes/products')
+app.use('/products', productRoutes)
+
 app.get('/', (req, res) => {
 	res.send('WIP Storefront Lite is running')
 })
